ba595d1f7ed185f48e87828e08f76725ba6ed4bc
Lukas <lukas@dutchie.org>
2018-02-06 14:51:51 +0100

    Kerstboom opdracht afgerond.

--- Opdracht 1/Busreis.php
+++ Opdracht 1/Busreis.php
@@ -14,15 +14,19 @@ $b = " Euro entree betalen.";
 
 if ($leeftijd > 0 && $leeftijd < 4){
     echo $a . 0 . $b;
-} elseif($leeftijd >= 65){
+}
+elseif($leeftijd >= 65){
     echo $a . $prijs / 2 . $b;
-} elseif ($leeftijd > 3 && $leeftijd < 13){
+}
+elseif ($leeftijd > 3 && $leeftijd < 13){
     echo $a . $prijs / 2 . $b;
-} elseif ($leeftijd > 12 && $leeftijd < 65){
+}
+elseif ($leeftijd > 12 && $leeftijd < 65){
     echo $a . $prijs . $b;
 }
 
 ?>
+
 <form action="" method="get">
     <input name="leeftijd" id="leeftijd" type="text" placeholder="Voer uw leeftijd in.">
     <input type="submit" formmethod="get">
